a9691ecc652ae7b3dfb99431c524c958f78e850c
Samantha <samantha.ming@gmail.com>
2016-04-22 00:31:01 -0700

    separated into functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,79 @@
 
 
-
 $( document ).ready(function() {
 
-    function GetMonthName(monthNumber) {
-      var months = ['January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'];
-      return months[monthNumber];
-    }
+  /***************************
+   * FUNCTIONS
+   ***************************/
 
+  function GetMonthName(monthNumber) {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[monthNumber];
+  }
+
+  function dayIndex(month){
+    var first_day = new Date(GetMonthName(month) + ' ' + 1 + ' ' + year).toDateString().substring(0, 3);
+    return day_name.indexOf(first_day);
+  }
+
+  function lastDate(year, month){
+    return new Date(year, month + 1, 0).getDate();
+  }
+
+  function calendar(day_index, last_date){
+    var counter = 0;
+    var $calendarContent = $('.calendar-content');
+
+    // clear calendar
+    $calendarContent.html("");
+
+    // First row
+    $calendarContent.append('<div class="calendar-row">');
+    for(var ir = 0; ir < 7; ir++ ){
+
+      if(ir == day_index || counter > 0){
+        counter++;
+        $calendarContent.append('<div class="calendar-column">' + counter + "</div>");
+
+      }else{
+        $calendarContent.append('<div class="calendar-column">' + '<span class="invisible">0</span>' + "</div>");
+      }
+    }
+    $calendarContent.append('</div>');
+
+    // Rest of the row
+    while (counter < last_date) {
+      $calendarContent.append('<div class="calendar-row">');
+      for(var nr = 0; nr < 7; nr++ ){
+        counter++;
+        if(counter <= last_date){
+          $calendarContent.append('<div class="calendar-column">' + counter + "</div>");
+        }else{
+          $calendarContent.append('<div class="calendar-column">' + '<span class="invisible">0</span>' + "</div>");
+        }
+      }
+      $calendarContent.append('</div>');
+    }
+  }
+
+  function calendarTitle(month, year){
+    $('.calendar-header-title').html(GetMonthName(month) + " " + year);
+  }
+
+  /***************************
+   * VARIABLES
+   ***************************/
 
     var day_name = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-
-
     var date = new Date();
     var year = date.getFullYear();
     var month = date.getMonth();
-
-
-    function dayIndex(month){
-        var first_day = new Date(GetMonthName(month) + ' ' + 1 + ' ' + year).toDateString().substring(0, 3);
-        return day_name.indexOf(first_day);
-    }
-
-    function lastDate(year, month){
-      return new Date(year, month + 1, 0).getDate();
-    }
-
-
-    // var first_day = firs tDay(month);
     var day_index = dayIndex(month);
     var last_date = lastDate(year, month);
 
-    // Header
-
-    $('.calendar-header-title').html(GetMonthName(month) + " " + year);
+    calendarTitle(month, year);
+    calendar(day_index, last_date);
 
     $('#next-month').on('click', function(e) {
       e.preventDefault();
@@ -42,8 +81,7 @@ $( document ).ready(function() {
         month ++;
         day_index = dayIndex(month);
         last_date = lastDate(year, month);
-        console.log(day_index);
-        $('.calendar-header-title').html(GetMonthName(month) + " " + year);
+        calendarTitle(month, year);
         calendar(day_index, last_date);
       }
     });
@@ -54,48 +92,9 @@ $( document ).ready(function() {
         month --;
         day_index = dayIndex(month);
         last_date = lastDate(year, month);
-        console.log(day_index);
-        $('.calendar-header-title').html(GetMonthName(month) + " " + year);
+        calendarTitle(month, year);
         calendar(day_index, last_date);
       }
     });
-
-
-    // Days
-
-    function calendar(day_index, last_date){
-      var counter = 0;
-      var $calendarContent = $('.calendar-content');
-
-      // clear calendar
-      $calendarContent.html("");
-
-      $calendarContent.append('<div class="calendar-row">');
-      for(var i = 0; i < 7; i++ ){
-
-        if(i == day_index || counter > 0){
-          counter++;
-          $calendarContent.append('<div class="calendar-column">' + counter + "</div>");
-
-        }else{
-          $calendarContent.append('<div class="calendar-column">' + counter + "</div>");
-        }
-      }
-      $calendarContent.append('</div>');
-
-      // Rest of the row
-      while (counter < last_date) {
-        $calendarContent.append('<div class="calendar-row">');
-        for(var nr = 0; nr < 7; nr++ ){
-          counter++;
-          $calendarContent.append('<div class="calendar-column">' + counter + "</div>");
-        }
-        $calendarContent.append('</div>');
-      }
-    }
-
-    calendar(day_index, last_date);
-
-
 
 });
